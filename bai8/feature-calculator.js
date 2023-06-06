567a3efdf63144ea852157a3c49c346a34314520
Nguyen Ha <127592129+nguyenha9889@users.noreply.github.com>
2023-06-06 15:16:58 +0700

    Update feature-calculator.js

--- bai8/feature-calculator.js
+++ bai8/feature-calculator.js
@@ -1,9 +1,34 @@
-const arrButtons = document.getElementsByTagName("button");
+const arrNumbers = document.getElementsByClassName("number");
+const arrOperators = document.getElementsByClassName("operator");
+const btnCalculate = document.getElementById("calculate");
 const result = document.getElementById("result");
-
-for (let i = 0; i < arrButtons; i++) {
-      arrButtons[i].addEventListener("click", () => {
-            result.innerText += arrButtons[i].value;
-      });
-      
+const btnReset = document.getElementById("reset");
+result.innerText = "0";
+function display(val) {
+	if (result.innerText == "0") {
+            result.innerText = "";
+            result.innerText += val;
+      } else result.innerText += val;
 }
+
+for (let i = 0; i < arrNumbers.length; i++) {
+	arrNumbers.item(i).addEventListener("click", () => {
+		display(arrNumbers.item(i).innerText);
+	});
+}
+
+for (let i = 0; i < arrOperators.length; i++) {
+	arrOperators.item(i).addEventListener("click", () => {
+		display(arrOperators.item(i).innerText);
+	});
+}
+
+btnCalculate.addEventListener("click", () => {
+	let y = eval(result.innerText);
+      console.log("text trong result ---> " + result.innerText);
+	result.innerText = y;
+});
+
+btnReset.addEventListener("click", () => {
+      result.innerText = "0";
+})
